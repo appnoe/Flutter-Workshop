6be890d8fc5830bbeff79d29cf1b9693887cc53a
Pascal Friedrich <info@pfriedrich.io>
2022-04-15 14:52:39 +0200

    Add localization example

---
+++ localization/lib/common/localizations.dart
@@ -0,0 +1,10 @@
+class SampleLocalizations {
+  static const mainView = MainView();
+}
+
+class MainView {
+  const MainView();
+  String get title => 'mainview_title';
+  String get subtitle => 'mainview_subtitle';
+  String get exampleWithParams => 'mainview_example_with_params';
+}
---
+++ localization/lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
+
+import 'common/localizations.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+  runApp(
+    EasyLocalization(
+      child: const MyApp(),
+      supportedLocales: const [
+        Locale('de', 'DE'),
+      ],
+      fallbackLocale: const Locale('de', 'DE'),
+      path: 'assets/languages',
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(SampleLocalizations.mainView.title.tr()),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              SampleLocalizations.mainView.exampleWithParams
+                  .tr(args: [_counter.toString()]),
+            ),
+            Text(
+              SampleLocalizations.mainView.subtitle.tr(),
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
