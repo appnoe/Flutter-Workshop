8dc247cf1aa74b6d6696e8cda41569f88622b5c3
Klaus M. Rodewig <kmr@appnoe.com>
2022-06-28 12:39:04 +0200

    👍🏻 - add empty sample app
    Signed-off-by: Klaus M. Rodewig <kmr@appnoe.com>

---
+++ sample_app/sample_app/lib/foobar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Foobar extends StatelessWidget {
+  const Foobar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class Boofar extends StatefulWidget {
+  const Boofar({Key? key}) : super(key: key);
+
+  @override
+  State<Boofar> createState() => _BoofarState();
+}
+
+class _BoofarState extends State<Boofar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
